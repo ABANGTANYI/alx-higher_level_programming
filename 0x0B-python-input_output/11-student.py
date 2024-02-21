9167bd3da8f09890ab0749daf402b51d450fcc3d
ABANGTANYI <abangtanyia@gmai.com>
2024-02-20 16:40:33 -0800

    Task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""method for student creation"""
-
-
 class Student:
-    """Student obj"""
-
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        return self.__dict__
+    def to_json(self, attrs=None):
+        if attrs is None or not isinstance(attrs, list):
+            return self.__dict__
+        return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+
+    def reload_from_json(self, json):
+        for attr, value in json.items():
+            setattr(self, attr, value)
