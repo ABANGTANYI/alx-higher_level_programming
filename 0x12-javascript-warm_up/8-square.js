bba83b195a773f67dd56d48021e5a082a5034e1f
ABANGTANYI <abangtanyia@gmai.com>
2024-04-28 13:19:07 -0700

    Task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,15 +1,11 @@
-#!/usr/bin/env node
-
-const size = parseInt(process.argv[2]);
-
+#!/usr/bin/node
+const size = Math.floor(Number(process.argv[2]));
 if (isNaN(size)) {
-  console.log("Missing size");
-} else if (size > 0) {
-  for (let i = 0; i < size; i++) {
-    let row = "";
-    for (let j = 0; j < size; j++) {
-      row += "X";
-    }
+  console.log('Missing size');
+} else {
+  for (let r = 0; r < size; r++) {
+    let row = '';
+    for (let c = 0; c < size; c++) row += 'X';
     console.log(row);
   }
 }
